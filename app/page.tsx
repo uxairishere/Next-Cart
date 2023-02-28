74a9b31a3b6d7ea795faca0409b88bd9571b25dd
Uxair Abbas <uzaaaaaair@gmail.com>
2023-03-01 02:34:40 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import BsFillPersonFill from 'react-icons'
+import profile from './assets/profile2.jpg'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -30,61 +33,25 @@ export default function Home() {
           </a>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+      <div className='grid grid-cols-3 gap-x-2 gap-y-10 w-3/4'>
+        <div className='rounded-lg '>
+          <Image src={profile} alt="..." width="200" className='float-right mr-[2rem] rounded-full' />
         </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className='rounded-lg col-span-2'>
+          <h1 className='my-[1rem] text-3xl font-bold'>Uxair Abbas</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique nam minima doloremque, doloribus tempore modi cumque commodi suscipit perspiciatis, consequuntur ut nemo laborum asperiores unde temporibus molestiae dicta id et!</p>
+          <button className="bg-gray-800 mt-[1rem] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            Portfolio
+          </button>
+        </div>
+        <div className='rounded-lg col-span-3'>
+          <div className={styles.description}>
+            <p>
+              Get started by editing&nbsp;
+              <code className={styles.code}>app/page.tsx</code>
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   )
